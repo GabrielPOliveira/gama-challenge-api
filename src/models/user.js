742f6a26f1cc081355a6e833d7abbd9cc3a1f66e
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-05-31 01:30:20 -0300

    Validação com Yup e criação da coluna "type"

--- src/models/user.js
+++ src/models/user.js
@@ -13,35 +13,25 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-        uuid: {
+      uuid: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4
       },
       login: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
-        validate: {
-          len: {args:[0, 20], msg: 'Login deve ter até 20 caracteres'}
-        },
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          notNull: {msg: 'Usuário precisa de uma senha'},
-          len: {
-            args: [8, 100], 
-            msg: 'Senha deve ter no mínimo 8 caracteres'},          
-        }
       },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          notNull: {msg: 'Usuário precisa de uma nome'},
-          notEmpty: {msg: 'Nome não pode ser vazio'},          
-        }
+      },
+      type: {
+        type: DataTypes.INTEGER,
+        allowNull: false
       },
   }, {
     sequelize,
