4405a7a7d4e880d6b8381496227fabeb87fd0570
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-06-09 18:47:16 -0300

    Producao funcional (#26)
    aplicação funcional

--- src/config/config.js
+++ src/config/config.js
@@ -18,10 +18,13 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
